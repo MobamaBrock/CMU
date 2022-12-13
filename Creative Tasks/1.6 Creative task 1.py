3a2bee392495804612b1616072bb350ee7927aec
Mobama Brock <91347579+MobamaBrock@users.noreply.github.com>
2022-12-12 21:12:22 -0800

    Create 1.6 Creative task 1.py

---
+++ Creative Tasks/1.6 Creative task 1.py
@@ -0,0 +1,8 @@
+Rect(0,80,400,250,fill='grey',border='black',borderWidth=10)
+Rect(18, 100, 365, 200, fill=gradient("red",'red','white','red','white','red'))
+Image('http://assets.stickpng.com/thumbs/580b57fbd9996e24bc43c0de.png', -5, 100)
+Rect(12,300,365,21,fill='grey')
+Rect(0,318,400,12)
+Rect(0,330,400,130,fill='white')
+Line(180,360,210,360,lineWidth=80)
+Label("Gotta Drink Coke.", 200, 200, fill='white', size=45,border = 'black',borderWidth = 1)
