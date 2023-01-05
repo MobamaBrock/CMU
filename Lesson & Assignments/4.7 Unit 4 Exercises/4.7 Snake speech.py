616fcf5a5f12e4fec63d62c279b5d1fad02fd326
Mobama Brock <91347579+MobamaBrock@users.noreply.github.com>
2023-01-05 10:20:50 -0800

    Update 4.7 Snake speech.py

--- Lesson & Assignments/4.7 Unit 4 Exercises/4.7 Snake speech.py
+++ Lesson & Assignments/4.7 Unit 4 Exercises/4.7 Snake speech.py
@@ -34,38 +34,3 @@ def makeSnakeSpeech():
 
 def onMousePress(mouseX, mouseY):
     makeSnakeSpeech()
----------------------------------------LAVA LAMP
-app.background = gradient('yellow', 'crimson', start='top')
-app.stepsPerSecond = 20
-
-
-# window
-Line(200, 0, 200, 400, lineWidth=4)
-Line(0, 200, 400, 200, lineWidth=4)
-Rect(0, 0, 400, 400, fill=None, border='black', borderWidth=10)
-
-# table
-Rect(100, 375, 200, 10)
-Rect(100, 385, 10, 15)
-Rect(290, 385, 10, 15)
-
-# Initialize empty blob group.
-blobs = []
-
-def createBlobs():
-    newBlobRadius = 5
-    newBlobX = 160
-    newBlobSpeed = 6
-
-    # Creates 6 blobs that increase in size and decrease in speed.
-    for i in range(6):
-        blob = Circle(newBlobX, 200, newBlobRadius, fill='red', opacity=90)
-        blob.dy = newBlobSpeed
-        blobs.insert(0,blob)
-
-        newBlobRadius += 5
-        newBlobX += 10
-        newBlobSpeed -= 1
-
-# Populates the blob group.
-createBlobs()
